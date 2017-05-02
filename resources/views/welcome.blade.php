bed8bfc2e5be97fd29a75b251afe94a184488d1c
Unknown <oDestroyeRo@hotmail.com>
2017-05-02 21:54:14 +0700

    first laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="text-center">The Wongklom Singer</h1>
+        </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://unsplash.it/200?image=0" alt="Sample Image">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://unsplash.it/200?image=1" alt="Sample Image">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://unsplash.it/200?image=2" alt="Sample Image">
+        </a>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <a href="#" class="thumbnail">
+          <img src="https://unsplash.it/200?image=3" alt="Sample Image">
+        </a>
+      </div>
+    </div>
+@endsection
