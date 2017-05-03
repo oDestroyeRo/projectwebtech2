a2f95185de3f587fb2643e504ea7c9e669057b40
Unknown <warinthorn.p@ku.th>
2017-05-03 20:38:20 +0700

    create all database table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->charset = 'utf8';
-            $table->collation = 'utf8_general_ci';
-            $table->increments('id');
-            $table->string('name');
+            $table->string('user_id')->primary();
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('tel');
+            $table->string('birthday');
+            $table->string('gender');
+            $table->text('address');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
