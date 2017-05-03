ec183495deabef80967aa0f2c193cbfa132bffcd
Unknown <warinthorn.p@ku.th>
2017-05-03 21:26:22 +0700

    edit app/Product file

--- app/Product.php
+++ app/Product.php
@@ -8,5 +8,5 @@ class Product extends Model
 {
     protected $fillable = ['product_id', 'product_img', 'product_name', 'product_price'];
     protected $visible = ['product_price', 'product_img', 'product_name', 'product_id'];
-    
+
 }
