527777df6ff7da932bac0b8bfa6fa543310759f8
5610450993 <pannawat.ka@ku.th>
2017-05-06 23:51:16 +0700

    edit file admin/create

--- resources/views/admin/promotion/create.blade.php
+++ resources/views/admin/promotion/create.blade.php
@@ -13,19 +13,31 @@
             <h4 class="modal-title" id="myModalLabel">Promotion</h4>
           </div>
           <div class="modal-body">
-            <form method="post" action="/promotion/insert"  enctype="multipart/form-data">
+            <form class="form-horizontal" method="post" action="/promotion/insert"  enctype="multipart/form-data">
               {{ csrf_field() }}
               <div class="form-group">
-                <label class="form-control"><?php  $promotion_id = \App\Promotion::max('promotion_id'); echo $promotion_id+1; ?></label>
+                <label for="id" class="col-md-3 control-label">ID</label>
+                <div class="col-md-8">
+                  <label for="id" class="form-control"><?php  $promotion_id = \App\Promotion::max('promotion_id'); echo $promotion_id+1; ?></label>
+                </div>
               </div>
               <div class="form-group">
-                <input type="file" class="form-control" name="promotionImg" id="promotionImg" placeholder="img">
+                <label for="img" class="col-md-3 control-label">BROWSE</label>
+                <div class="col-md-8">
+                  <input for="img" type="file" class="form-control" name="promotionImg" id="promotionImg" placeholder="img">
+                </div>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" name="promotionDes" id="promotionDes" placeholder="Description">
+                <label for="description" class="col-md-3 control-label">DESCRIPTION</label>
+                <div class="col-md-8">
+                  <input for="description" type="text" class="form-control" name="promotionDes" id="promotionDes" placeholder="Description">
+                </div>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" name="promotionDis" id="promotionDis" placeholder="Discount">
+                <label for="discount" class="col-md-3 control-label">DISCOUNT</label>
+                <div class="col-md-8">
+                  <input for="discount" type="text" class="form-control" name="promotionDis" id="promotionDis" placeholder="Discount">
+                </div>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
