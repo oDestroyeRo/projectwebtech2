a65e9a26448e61108f8a996841fc0ab20369a1e0
Kanpitcha Wongsawang <kanpitcha.w@ku.th>
2017-05-07 16:05:16 +0700

    edit peofile customer and update database

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="jumbotron">
+        <div class="container">
+
+            <h1 class="text-center">Profile</h1>
+        </div>
+    </div>
+    @foreach($data as $a)
+    <div class="row">
+      <div class="col-xs-6 col-md-4">
+                <center> <p>id : {{$a->id}} </p> </center>
+                <center><label>Firstname :  <input type='text' placeholder="{{$a->firstname}}"></label></center>
+                <center> <label>Lastname : <input type='text' placeholder="{{$a->lastname}}"></label></center>
+                <center> <label>Tel : <input type='text' placeholder="{{$a->tel}}"></label></center>
+                <center> <label>Address : <input type='text' placeholder="{{$a->address}}"></label></center>
+                <center> <label>Change password : <input type='password' ></input></label></center>
+                <center><input type='submit' value='submit' onclick={{route('editProfile')}}></button></center>
+      </div>
+    @endforeach
+@endsection
