453930a3bae242d8d9392bb753bc7e56b61e6f84
MomayTH <waranya.th@ku.th>
2017-05-07 15:48:00 +0700

    add layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Coffee cafe</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template -->
-    <link href="css/business-casual.css" rel="stylesheet">
-
-    <!-- Temporary navbar container fix -->
-    <style>
-    .navbar-toggler {
-        z-index: 1;
-    }
-    
-    @media (max-width: 576px) {
-        nav > .container {
-            width: 100%;
-        }
-    }
-    /* Temporary fix for img-fluid sizing within the carousel */
-    
-    .carousel-item.active,
-    .carousel-item-next,
-    .carousel-item-prev {
-        display: block;
-    }
-    </style>
-
-</head>
-
-<body>
-
-    <div class="tagline-upper text-center text-heading text-shadow text-white mt-4 hidden-md-down">KK Coffee</div>
-    <div class="tagline-lower text-center text-expanded text-shadow text-uppercase text-white mb-4 hidden-md-down">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-toggleable-md navbar-light navbar-custom bg-faded py-lg-4">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="container">
-            <a class="navbar-brand text-uppercase text-expanded font-weight-bold hidden-lg-up" href="#">Start Bootstrap</a>
-            <div class="collapse navbar-collapse" id="navbarExample">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item active px-lg-4">
-                        <a class="nav-link text-uppercase text-expanded" href="/index">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item px-lg-4">
-                        <a class="nav-link text-uppercase text-expanded" href="/customer/menu">Register</a>
-                    </li>
-                    <li class="nav-item px-lg-4">
-                  
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container">
-
-        <div class="bg-faded p-4 my-4">
-            <!-- Image Carousel -->
-<!--  -->
-
-
+@extends('layouts.app')
+@section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-4">
             <div class="panel panel-default">
 
                 <div class="panel-body">
@@ -140,28 +64,4 @@
         </div>
     </div>
 </div>
-
-
-            </div>
-            <!-- Welcome Message -->
-        </div>
-
- 
-    </div>
-    <!-- /.container -->
-
-    <footer class="bg-faded text-center py-5">
-        <div class="container">
-            <p class="m-0">Copyright &copy; Your Website 2017</p>
-        </div>
-    </footer>
-
-
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/tether/tether.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-
-</html>
-
+@endsection
