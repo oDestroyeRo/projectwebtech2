11e6b9ff51e52553558f5f7242bb0010d649d5cc
my_jabz <kemika.t@ku.th>
2017-05-08 19:39:48 +0700

    edit profile(complete)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,44 +1,141 @@
-
 @extends('layouts.app')
 
-
 @section('content')
-    <div class="jumbotron">
-        <div class="container">
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('profile.update',Auth::id()) }}">
+                        {{ csrf_field() }}
+                        @foreach ($data as $d)
+                        <div class="form-group">
+                            <label for="id" class="col-md-4 control-label">ID</label>
 
-            <h1 class="text-center">Profile</h1>
+                            <div class="col-md-6">
+                                  <label for="id" class="col-md-4 control-label"><?php  $id = \App\User::max('id'); echo $id+1; ?></label>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                            <label for="firstname" class="col-md-4 control-label">Firstname</label>
+
+                            <div class="col-md-6">
+                                <input id="firstname" type="text" class="form-control" name="firstname" value="{{ $d->firstname }}" required autofocus>
+
+                                @if ($errors->has('firstname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('firstname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                            <label for="lastname" class="col-md-4 control-label">Lastname</label>
+
+                            <div class="col-md-6">
+                                <input id="lastname" type="text" class="form-control" name="lastname" value="{{ $d->lastname }}" required autofocus>
+
+                                @if ($errors->has('lastname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('lastname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $d->email }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <!-- <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">Address</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required autofocus>
+
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div> -->
+                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">Address</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="tel" class="form-control" name="address" value="{{ $d->address }}" required autofocus>
+
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('tel') ? ' has-error' : '' }}">
+                            <label for="tel" class="col-md-4 control-label">Tel</label>
+
+                            <div class="col-md-6">
+                                <input id="tel" type="tel" class="form-control" name="tel" value="{{ $d->tel }}" required autofocus>
+
+                                @if ($errors->has('tel'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('tel') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">New Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    save
+                                </button>
+                            </div>
+                        </div>
+                        @endforeach
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-    <form class="form-horizontal" role="form" method="post" action="{{ route('profile.update',Auth::user()->id) }}">
-        {{ csrf_field() }}
-    @foreach($data as $a)
-    <div class="row">
-      <div class="col-xs-6 col-md-4">
-                <center> <p>id : {{$a->id}} </p> </center>
-                <center><label>Firstname :  <input id='firstname' name='firstname' type='text' value="{{$a->firstname}}"></label></center>
-                <center> <label>Lastname : <input id='lastname' name='lastname'  type='text' value="{{$a->lastname}}"></label></center>
-                <center> <label>Tel : <input id='tel' name='tel'  type='text' value="{{$a->tel}}"></label></center>
-                <center> <label>Address : <input id='address' name='address'  type='text' value="{{$a->address}}"></label></center>
-                <center> <label>Change password : <input id='pass' name='pass' type='password' ></input></label></center>
-                <center><input type='submit' value='submit'></button></center>
-      </div>
-    @endforeach
-  </form>
-
-    <!-- <form method="POST" action="">
-  <input type="hidden" name="_token" value="">
-  <input type="hidden" name="_method" value="PUT">
-  <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" name="name" value="" class="form-control">
-  </div>
-  <div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" name="email" value="" class="form-control">
-  </div>
-  <button type="submit" class="btn btn-primary">
-    <i class="fa fa-btn fa-sign-in"></i>Update
-  </button>
-</form> -->
-
+</div>
 @endsection
