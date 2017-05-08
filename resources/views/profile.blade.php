b0c6afbe1c6cb07d36bab0f761ea51e915ad7c05
my_jabz <kemika.t@ku.th>
2017-05-08 14:55:00 +0700

    ediot profile page(complete)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,20 +9,23 @@
             <h1 class="text-center">Profile</h1>
         </div>
     </div>
-  <!--   @foreach($data as $a)
+    <form class="form-horizontal" role="form" method="post" action="{{ route('profile.update',Auth::user()->id) }}">
+        {{ csrf_field() }}
+    @foreach($data as $a)
     <div class="row">
       <div class="col-xs-6 col-md-4">
                 <center> <p>id : {{$a->id}} </p> </center>
-                <center><label>Firstname :  <input type='text' placeholder="{{$a->firstname}}"></label></center>
-                <center> <label>Lastname : <input type='text' placeholder="{{$a->lastname}}"></label></center>
-                <center> <label>Tel : <input type='text' placeholder="{{$a->tel}}"></label></center>
-                <center> <label>Address : <input type='text' placeholder="{{$a->address}}"></label></center>
-                <center> <label>Change password : <input type='password' ></input></label></center>
-                <center><input type='submit' value='submit' onclick=></button></center>
+                <center><label>Firstname :  <input id='firstname' name='firstname' type='text' value="{{$a->firstname}}"></label></center>
+                <center> <label>Lastname : <input id='lastname' name='lastname'  type='text' value="{{$a->lastname}}"></label></center>
+                <center> <label>Tel : <input id='tel' name='tel'  type='text' value="{{$a->tel}}"></label></center>
+                <center> <label>Address : <input id='address' name='address'  type='text' value="{{$a->address}}"></label></center>
+                <center> <label>Change password : <input id='pass' name='pass' type='password' ></input></label></center>
+                <center><input type='submit' value='submit'></button></center>
       </div>
-    @endforeach -->
+    @endforeach
+  </form>
 
-    <form method="POST" action="">
+    <!-- <form method="POST" action="">
   <input type="hidden" name="_token" value="">
   <input type="hidden" name="_method" value="PUT">
   <div class="form-group">
@@ -36,6 +39,6 @@
   <button type="submit" class="btn btn-primary">
     <i class="fa fa-btn fa-sign-in"></i>Update
   </button>
-</form>
+</form> -->
 
 @endsection
